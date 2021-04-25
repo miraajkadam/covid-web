cb76bb0781ec8c625cbeb1015b76ea33de776b67
Miraaj Kadam <miraajkadam@gmail.com>
2021-04-25 12:45:07 +0530

    add and refactor both covid messages

--- webapp/views.py
+++ webapp/views.py
@@ -15,6 +15,66 @@ from pathlib import Path
 
 # Create your views here.
 
+
+def cov_pos_msg(name, prob_ar):
+    return f'Hello {name}, I am sorry to tell you this but your X-Ray has been classified as covid positive. You would need to get tested from a covid facility to get ensured about the results where you would be informed about the further steps.\
+            \
+            \
+            \n\nYour X-Rays were classified with following probablities:\
+            \nNormal: {prob_ar[0]}\
+            \nViral: {prob_ar[1]}\
+            \nCovid: {prob_ar[2]}\
+            \
+            \n\nMentioned below are some important ways to help keep you and your family safe.\
+            \n1. Stay home except to get medical care.\
+            \n2. Separate yourself from other people.\
+            \n3. Monitor your symptoms.\
+            \n4. Call ahead before visiting your doctor.\
+            \n5. Cover your coughs and sneezes.\
+            \n6. Clean your hands often.\
+            \n7. Avoid sharing personal household items.\
+            \n8. Clean all "high-touch" surfaces everyday.\
+            \
+            \n\nPlease go through some of links to get more important information.\
+            \nCDC Guidelines on \"What to Do If You Are Sick\" - https://www.cdc.gov/coronavirus/2019-ncov/if-you-are-sick/steps-when-sick.html\
+            \nCDC Guidelines on \"Caring for Someone Sick at Home\" - https://www.cdc.gov/coronavirus/2019-ncov/if-you-are-sick/care-for-someone.html\
+            \
+            \n\nStay safe and healthy...!\
+            \
+            \
+            \n\n\nRegards,\
+            \nMiraaj, Neeraj, Paras\
+            \nGroup 4'
+
+
+def cov_neg_msg(name, prob_ar):
+    return f'Hello {name}, You are covid negative, but being negative doesn\'t ensures that you will stay negative.\
+            \
+            \
+            \n\nYour X-Rays were classified with following probablities:\
+            \nNormal: {prob_ar[0]}\
+            \nViral: {prob_ar[1]}\
+            \nCovid: {prob_ar[2]}\
+            \
+            \n\nMentioned below are some important ways to help keep you and your family safe.\
+            \n1. Wear a mask that covers your nose and mouth to help protect yourself and others.\
+            \n2. Stay 6 feet apart from others who don’t live with you.\
+            \n3. Get a COVID-19 vaccine when it is available to you.\
+            \n4. Avoid crowds and poorly ventilated indoor spaces.\
+            \n5. Wash your hands often with soap and water. Use hand sanitizer if soap and water aren’t available.\
+            \
+            \n\nPlease go through some of links to get more important information.\
+            \nCDC Guidelines on \"What to Do If You Are Sick\" - https://www.cdc.gov/coronavirus/2019-ncov/if-you-are-sick/steps-when-sick.html\
+            \nCDC Guidelines on \"Caring for Someone Sick at Home\" - https://www.cdc.gov/coronavirus/2019-ncov/if-you-are-sick/care-for-someone.html\
+            \
+            \n\nStay safe and healthy...!\
+            \
+            \
+            \n\n\nRegards,\
+            \nMiraaj, Neeraj, Paras\
+            \nGroup 4'
+
+
 class_names = ['normal', 'viral', 'covid']
 
 resnet18 = torchvision.models.resnet18(pretrained=True)
@@ -56,6 +116,7 @@ def predict_image_class(image_path):
 def index(request):
     if(request.method == "POST"):
         email = request.POST.get('email')
+        name = request.POST.get('name')
         file = request.FILES['imageFile']
         file_name = default_storage.save(file.name, file)
         file_url = default_storage.path(file_name)
@@ -66,32 +127,15 @@ def index(request):
 
         if(predicted_class_index == 2):
             positive = True
-            str = "You are positive"
+            message = cov_pos_msg(name, prob_ar)
             send_mail('You are Positive!',
-                      str,
+                      message,
                       'Team Covid', [email],
                       fail_silently=False,
                       html_message=None)
         else:
             positive = False
-            message = "You are covid negative, but being negative doesn't ensures that you will stay negative.\
-                \n\nMentioned below are some important ways to help keep you and your family safe.\
-                \n1. Wear a mask that covers your nose and mouth to help protect yourself and others.\
-                \n2. Stay 6 feet apart from others who don’t live with you.\
-                \n3. Get a COVID-19 vaccine when it is available to you.\
-                \n4. Avoid crowds and poorly ventilated indoor spaces.\
-                \n5. Wash your hands often with soap and water. Use hand sanitizer if soap and water aren’t available.\
-                \
-                \n\nPlease go through some of links to get more important information.\
-                \nCDC Guidelines on \"What to Do If You Are Sick\" - https://www.cdc.gov/coronavirus/2019-ncov/if-you-are-sick/steps-when-sick.html\
-                \nCDC Guidelines on \"Caring for Someone Sick at Home\" - https://www.cdc.gov/coronavirus/2019-ncov/if-you-are-sick/care-for-someone.html\
-                \
-                \n\nStay safe and healthy...!\
-                \
-                \
-                \n\n\nRegards,\
-                \nMiraaj, Neeraj, Paras\
-                \nTeam Covid"
+            message = cov_neg_msg(name, prob_ar)
 
             send_mail('You are negative!',
                       message,
